a0c4e9af38e73e10d91d723ae974e6eae723fff2
Baycove <60710333+Baycove@users.noreply.github.com>
2020-03-09 12:59:55 -0600

    School App Breif and Basic Design

---
+++ School App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  School App
+//
+//  Created by Samantha De Campos on 2020-03-04.
+//  Copyright © 2020 Samantha De Campos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
